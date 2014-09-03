356fa640e7dff5b341d37b0e6b1af3ed8e6630d8
egbbr <eduardogbsouza@gmail.com>
2014-09-03 16:41:49 -0300

    ControleFinanceiro
    Treino Windons Forms

---
+++ ControleFinanceiro/Domain/Evento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.Domain
+{
+    public class Evento
+    {
+        public int EventoID { get; set; }
+        public DateTime CompetenciaPagamento { get; set; }
+    }
+}
---
+++ ControleFinanceiro/Domain/FormaCobranca.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.Domain
+{
+    public class FormaCobranca
+    {
+        public short FormaCobrancaID { get; set; }
+        public string Descricao { get; set; }
+        public Banco Banco { get; set; }
+    }
+
+    public class Banco
+    {
+        public short BancoID { get; set; }
+        public string Descricao { get; set; }
+    }
+}
